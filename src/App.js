061ea325834823aeee2626dfc227ad84d80ac8eb
Stephen <stephenadeyemo@gmail.com>
2022-07-13 21:38:17 +0100

    designing a todo app with reactjs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import TaskManager from "./components/taskManager/TaskManager";
+
+function App() {
+  return (
+    <div>
+      <TaskManager/>
+    </div>
+  );
+}
+
+export default App;
